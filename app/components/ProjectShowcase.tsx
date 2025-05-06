9d68442768dbc910d40110e8a2d344660b5c6b48
YoussefBenSlimene <benslimeneyoussef@mahdia.r-iset.tn>
2025-05-06 21:59:10 +0100

    dqjsd

--- app/components/ProjectShowcase.tsx
+++ app/components/ProjectShowcase.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import ImageCarousel from "./ImageCarousel";
-// Import only the Carousel CSS instead of all Bootstrap CSS
 
 interface ProjectShowcaseProps {
   name: string;
